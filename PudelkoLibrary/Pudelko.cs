e43a829397cc4dbaae5165b38ee4cc5f55591314
K4thos <piotrpit.uek@gmail.com>
2022-04-28 11:15:50 +0200

    Add Pudelko constructor

--- PudelkoLibrary/Pudelko.cs
+++ PudelkoLibrary/Pudelko.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace PudelkoLibrary
 {
-    internal class Pudelko
+    public class Pudelko
     {
+        public double A;
+        public double B;
+        public double C;
+        private UnitOfMeasure Unit;
+
+        public Pudelko(double? a = null, double? b = null, double? c = null, UnitOfMeasure unit = UnitOfMeasure.meter)
+        {
+            // Default dimensions: 10 cm × 10 cm × 10 cm
+            double defaultDimension = 0.1;
+            switch (unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    defaultDimension = 100;
+                    break;
+                case UnitOfMeasure.centimeter:
+                    defaultDimension = 10;
+                    break;
+            }
+
+            // For internal use dimensions are stored as meters
+            Unit = UnitOfMeasure.meter;
+            A = UnitOfMeasureMethods.Convert(a ?? defaultDimension, unit, UnitOfMeasure.meter, 3);
+            B = UnitOfMeasureMethods.Convert(b ?? defaultDimension, unit, UnitOfMeasure.meter, 3);
+            C = UnitOfMeasureMethods.Convert(c ?? defaultDimension, unit, UnitOfMeasure.meter, 3);
+
+            // Creation exceptions
+            if (A <= 0 || B <= 0 || C <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Pudelko dimensions must be larger than 0");
+            }
+            if (A >= 10 || B >= 10 || C >= 10)
+            {
+                throw new ArgumentOutOfRangeException("Pudelko dimensions must be smaller than 10 m");
+            }
+        }
     }
 }
---
+++ PudelkoLibrary/UnitOfMeasure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PudelkoLibrary
+{
+    public enum UnitOfMeasure
+    {
+        milimeter,
+        centimeter,
+        meter
+    }
+
+    public class UnitOfMeasureMethods
+    {
+        /// <summary>
+        /// Converts value between units of measure
+        /// </summary>
+        /// <param name="value">Value to be converted</param>
+        /// <param name="fromUnit">Input unit of measure</param>
+        /// <param name="toUnit">Output unit of measure</param>
+        /// <param name="precision">Rounding precision</param>
+        /// <returns>Converted Value</returns>
+        public static double Convert(double val, UnitOfMeasure fromUnit, UnitOfMeasure toUnit, double precision = 3)
+        {
+            return Math.Floor(val * UnitMult(toUnit) / UnitMult(fromUnit) * Math.Pow(10, precision)) / Math.Pow(10, precision);
+        }
+
+        /// <summary>
+        /// Multiplier for units of measure conversions
+        /// </summary>
+        /// <param name="unit">Unit of measure</param>
+        /// <returns>Unit of measure multiplier</returns>
+        public static double UnitMult(UnitOfMeasure unit)
+        {
+            double mult = 1;
+            switch (unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    mult = 1000;
+                    break;
+                case UnitOfMeasure.centimeter:
+                    mult = 100;
+                    break;
+            }
+            return mult;
+        }
+    }
+}
